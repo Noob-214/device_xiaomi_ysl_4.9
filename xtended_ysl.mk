6d1d35a368a03bdb829749a2129e30795f3d1903
Sandeep Chahal <sandeeps567ingh@gmail.com>
2021-08-22 08:41:51 +0000

    ysl: fix missing apn

--- xtended_ysl.mk
+++ xtended_ysl.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
 # Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common.mk)
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit some common Xtended flags
 TARGET_BOOT_ANIMATION_RES := 720
