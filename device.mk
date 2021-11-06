afb1d564a7d2c6339fa4ae7aad0f8b4d266cce84
Sandeep Chahal <sandeeps567ingh@gmail.com>
2021-11-06 11:03:44 +0000

    ysl: hardcode hal paths[DNM]

--- device.mk
+++ device.mk
@@ -464,3 +464,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# hals
+SRC_MEDIA_HAL_DIR := hardware/qcom-caf/msm8996/media
+SRC_DISPLAY_HAL_DIR := hardware/qcom-caf/msm8996/display
+SRC_AUDIO_HAL_DIR := hardware/qcom-caf/msm8996/audio
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom-caf/msm8996
